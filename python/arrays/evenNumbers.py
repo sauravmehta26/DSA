80bd1313ce56f00e0caffd4453a1687b520c2598
sauravmehta26 <sauravmehta786@gmail.com>
2025-04-29 22:02:10 +0530

    space

--- python/arrays/evenNumbers.py
+++ python/arrays/evenNumbers.py
@@ -4,5 +4,6 @@ class Evennumbers:
             if i%2==0:
                 print(i, end=' ')
                 
+                
 n = list(map(int, input("Enter numbers separated by spaces: ").split()))
 Evennumbers.even(n)
